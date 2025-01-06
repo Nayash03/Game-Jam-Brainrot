c7b54c93c81333513c80d72deb57da2d5b9c4cb1
Nayash <shayan.cahelo@edu.esiee-it.fr>
2025-01-06 15:41:51 +0100

    add ennemy

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    private float Direction = 4;
+    
+
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    private Vector3 velocity = Vector3.zero;
+
+
+    void FixedUpdate()
+    {
+        
+        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+
+        MovePlayer(horizontalMovement, verticalMovement);
+
+        
+
+        if (Mathf.Abs(rb.velocity.x) > Mathf.Abs(rb.velocity.y))
+        {
+            float characterVelocity = Mathf.Abs(rb.velocity.x);
+            animator.SetFloat("Speed", characterVelocity);
+           
+        }
+        else
+        {
+            float characterVelocity = Mathf.Abs(rb.velocity.y);
+            animator.SetFloat("Speed", characterVelocity);
+        }
+        animator.SetFloat("Direction", Direction);
+    }
+
+    void MovePlayer(float _horizontalMovement, float _verticalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, _verticalMovement);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, 0.05f);
+
+        if (Mathf.Abs(rb.velocity.x) > Mathf.Abs(rb.velocity.y))
+        {
+            // Mouvement horizontal 
+            if (rb.velocity.x > 0.3f)
+            {
+                Direction = 3; // right
+            }
+            else if (rb.velocity.x < -0.3f)
+            {
+                Direction = 2; // left
+            }
+        }
+        else
+        {
+            // Mouvement vertical 
+            if (rb.velocity.y > 0.3f)
+            {
+                Direction = 1; // top
+            }
+            else if (rb.velocity.y < -0.3f)
+            {
+                Direction = 4; // down
+            }
+        }
+
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/WeakSpot.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using UnityEngine;
+
+public class WeakSpot : MonoBehaviour
+{
+    public GameObject objectToDestroy;
+    private bool playerInTrigger = false;
+
+    public Animator animator;
+    private bool dead = false;
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInTrigger = true; 
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInTrigger = false; 
+        }
+    }
+
+    private void Update()
+    {
+        
+       
+        if (playerInTrigger && Input.GetButtonDown("Fire1"))
+        {
+            dead = true;
+            animator.SetTrigger("dead");
+            
+            StartCoroutine(WaitAndExecute());
+            
+        }
+    }
+
+ 
+
+    IEnumerator WaitAndExecute()
+    {
+        // Attendre 1 seconde
+        yield return new WaitForSeconds(1.1f);
+
+        // Code à exécuter après 1 seconde
+        Destroy(objectToDestroy); 
+    }
+}
